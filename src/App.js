e76f8df30a243fca211d5eb8d3d87d842baf7081
Ronald <ronald@Ronalds-MacBook-Pro.local>
2023-09-11 15:13:40 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
-
+import { MovielistContainer } from './Component/MovielistContainer';
+import axios from 'axios';
+import styled from "styled-components"
+import { Movieinfocontainer } from './Component/Movieinfocontainer';
+export const API_KEY = "8f1b566c"
+const Placeholder = styled.img`
+  width: 120px;
+  height: 120px;
+  margin: 150px;
+  opacity: 50%;
+`;
 function App() {
+  const [searchquery, setsearchquery] = useState("")
+  const [timeout, setimeout] = useState()
+  const [movielist, setmovielist] = useState([])
+  const [selectedMovie, setselectedMovie] = useState()
+  const fetch = async (searchString) => {
+    const res = await axios.get(`https://www.omdbapi.com/?s=${searchString}&apikey=${API_KEY}`)
+    setmovielist(res.data.Search)
+  }
+  const handlechange = (e) => {
+    clearTimeout(timeout)
+    setsearchquery(e.target.value)
+    const timeup = setTimeout(() => {
+      fetch(e.target.value)
+    }, 500);
+    setimeout(timeup);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className='Header'>
+        <div className='appname'>
+          <img src="/movie-icon.svg" alt="movie" className='movieimage' />
+          Movie Apps
+        </div>
+        <div className='searchbox'>
+          <div className="searchicon">
+            <img src="/search-icon.svg" alt="icon" className="searchicon" />
+          </div>
+          <div className='searchinput'>
+            <input type="text" className='searchinput' value={searchquery} onChange={handlechange} placeholder='Search movie' />
+          </div>
+        </div>
+      </div>
+      <div>
+        {selectedMovie && <Movieinfocontainer selectedMovie={selectedMovie} setselectedMovie={setselectedMovie}/>}
+      </div>
+      <div className='movielistcontainer'>
+        {
+          movielist?.length ?   movielist.map((movie,index) => <MovielistContainer key={index} setselectedMovie={setselectedMovie} movie={movie}/>):<Placeholder src="/movie-icon.svg" />
+
+        }
+      </div>
     </div>
   );
 }
---
+++ src/Component/Movieinfocontainer.js
@@ -0,0 +1,123 @@
+import React, { useEffect, useState } from 'react';
+import styled from "styled-components";
+import Axios from 'axios';
+
+export const API_KEY = "8f1b566c";
+const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  padding: 20px 30px;
+  justify-content: center;
+  border-bottom: 1px solid lightgray;
+`;
+const CoverImage = styled.img`
+  object-fit: cover;
+  height: 350px;
+`;
+const InfoColumn = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 20px;
+`;
+const MovieName = styled.span`
+  font-size: 22px;
+  font-weight: 600;
+  color: black;
+  margin: 15px 0;
+  white-space: nowrap;
+  overflow: hidden;
+  text-transform: capitalize;
+  text-overflow: ellipsis;
+  & span {
+    opacity: 0.8;
+  }
+`;
+const MovieInfo = styled.span`
+  font-size: 16px;
+  font-weight: 500;
+  color: black;
+  overflow: hidden;
+  margin: 4px 0;
+  text-transform: capitalize;
+  text-overflow: ellipsis;
+
+  & span {
+    opacity: 0.5;
+  }
+`;
+const Close = styled.span`
+  font-size: 16px;
+  font-weight: 600;
+  color: black;
+  background: lightgray;
+  height: fit-content;
+  padding: 8px;
+  border-radius: 50%;
+  cursor: pointer;
+  opacity: 0.8;
+`;
+
+export const Movieinfocontainer = ({ selectedMovie,setselectedMovie }) => {
+  const [movie, setMovie] = useState({}); // Initialize movie as an empty object
+
+  useEffect(() => {
+    const fetchMovieData = async () => {
+      try {
+        const response = await Axios.get(`https://www.omdbapi.com/?i=${selectedMovie}&apikey=${API_KEY}`);
+        setMovie(response.data); // Update movie with the fetched data
+      } catch (error) {
+        console.error('Error fetching movie data:', error);
+      }
+    };
+
+    if (selectedMovie) {
+      fetchMovieData();
+    }
+  }, [selectedMovie]);
+
+  return (
+    <Container>
+      {movie ?(
+      <>
+        <CoverImage src={movie?.Poster} alt="Movie Poster" />{/* Use movie?.Poster */}
+      <InfoColumn>
+            <MovieName>
+              {movie?.Type}: <span>{movie?.Title}</span>
+            </MovieName>
+            <MovieInfo>
+              IMDB Rating: <span>{movie?.imdbRating}</span>
+            </MovieInfo>
+            <MovieInfo>
+              Year: <span>{movie?.Year}</span>
+            </MovieInfo>
+            <MovieInfo>
+              Language: <span>{movie?.Language}</span>
+            </MovieInfo>
+            <MovieInfo>
+              Rated: <span>{movie?.Rated}</span>
+            </MovieInfo>
+            <MovieInfo>
+              Released: <span>{movie?.Released}</span>
+            </MovieInfo>
+            <MovieInfo>
+              Runtime: <span>{movie?.Runtime}</span>
+            </MovieInfo>
+            <MovieInfo>
+              Genre: <span>{movie?.Genre}</span>
+            </MovieInfo>
+            <MovieInfo>
+              Director: <span>{movie?.Director}</span>
+            </MovieInfo>
+            <MovieInfo>
+              Actors: <span>{movie?.Actors}</span>
+            </MovieInfo>
+            <MovieInfo>
+              Plot: <span>{movie?.Plot}</span>
+            </MovieInfo>
+          </InfoColumn>
+          <Close onClick={()=> setselectedMovie()}><span>x</span> </Close>
+      </>): "loading...."}
+      
+    </Container>
+  );
+};
